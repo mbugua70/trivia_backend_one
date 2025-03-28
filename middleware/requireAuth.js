632632a2df9754b084e276f90831d977a0852f5d
Code MBU <johnmbugua23code@gmail.com>
2025-03-28 14:20:43 +0300

    removed authentication

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,23 +1,23 @@
 const jwt = require("jsonwebtoken");
 const UserModal = require("../models/userLogin");
 
-const requireAuth = async (req, res, next) => {
-  const { authorization } = req.headers;
-  console.log(authorization);
+// const requireAuth = async (req, res, next) => {
+//   const { authorization } = req.headers;
+//   console.log(authorization);
 
-  if (!authorization) {
-    return res.status(401).json({ error: "Authorization token required" });
-  }
+//   if (!authorization) {
+//     return res.status(401).json({ error: "Authorization token required" });
+//   }
 
-  const token = authorization.split(" ")[1];
+//   const token = authorization.split(" ")[1];
 
-  try {
-    const { _id } = jwt.verify(token, process.env.SECRET_STRING);
-    req.user = await UserModal.findOne({ _id }).select("_id");
-    next();
-  } catch (err) {
-    res.status(401).json({ err: "Request is not authorized" });
-  }
-};
+//   try {
+//     const { _id } = jwt.verify(token, process.env.SECRET_STRING);
+//     req.user = await UserModal.findOne({ _id }).select("_id");
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ err: "Request is not authorized" });
+//   }
+// };
 
-module.exports = requireAuth;
+// module.exports = requireAuth;
